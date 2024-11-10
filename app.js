ad172a6e85c925f0c795549ee4ba05c51a51a779
AmirHosein Deldar <Amirhoseindeldar@yahoo.com>
2024-11-10 06:33:57 +0330

    adding aos animation

--- app.js
+++ app.js
@@ -21,3 +21,26 @@ $(".feedbacks-container").slick({
     },
   ],
 });
+
+const sections = document.querySelectorAll("section");
+const menuItems = document.querySelectorAll(".tabs .tab");
+menuItems[0].classList = () => {
+  sections.forEach((section) => {
+    let top = window.scrollY;
+    let offset = section.offsetTop - 100;
+    let height = section.offsetHeight;
+    let id = section.getAttribute("id");
+    if (top >= offset && top < offset + height) {
+      menuItems.forEach((item) => {
+        item.classList.remove("active");
+        document
+          .querySelector(".tabs a[href*=" + id + "]")
+          .classList.add("active");
+      });
+    }
+  });
+};
+AOS.init({
+  duration: 500,
+  easing: "ease-in-sine",
+});
